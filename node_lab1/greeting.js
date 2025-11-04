596c05b388533a75cd9c67819bb6205a3a275c40
Bobby92262 <bobbyfinn086@gmail.com>
2025-11-04 12:33:02 +0000

    Completed node lab 1!

--- node_lab1/greeting.js
+++ node_lab1/greeting.js
@@ -1,6 +1,10 @@
 const greeting = {
     en: "Hello World!",
-    fr: "Bonjour!"
+    fr: "Bonjour!",
+    ga: "Dia duit!",
+    'zh-Hans': "你好世界",
+    es: "Hola Mundo"
+
 };
 
 export default greeting;
